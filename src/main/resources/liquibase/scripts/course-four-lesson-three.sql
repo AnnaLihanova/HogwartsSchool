d2beb8656974346deda8359700a12af4f313f2e4
AnnaLihanova <anja.lihanova@gmail.com>
2022-06-30 17:15:46 +0300

    HW_4.3 правка 2

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -4,3 +4,8 @@
 CREATE INDEX student_name_index ON student (name);
 
 CREATE INDEX faculty_name_and_color_index ON faculty (name, color);
+
+-- changeset annaLihanova:2
+CREATE INDEX student_age_index ON student (age);
+
+DROP INDEX student_age_index;
